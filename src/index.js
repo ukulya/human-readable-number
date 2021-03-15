cab4fd235e6c141a029dda94c0d56dfc9935b90f
ukulya <ukulya150992@gmail.com>
2021-03-15 23:59:03 +0600

    feat: update file

--- src/index.js
+++ src/index.js
@@ -1,47 +1,63 @@
-module.exports = function numberToWords (number) {
-    var th_val = ['', 'thousand', 'million', 'billion', 'trillion'];
-var dg_val = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-var tn_val = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-var tw_val = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    s = s.toString();
-    s = s.replace(/[\, ]/g, '');
-    if (s != parseFloat(s))
-        return 'not a number ';
-    var x_val = s.indexOf('.');
-    if (x_val == -1)
-        x_val = s.length;
-    if (x_val > 15)
-        return 'too big';
-    var n_val = s.split('');
-    var str_val = '';
-    var sk_val = 0;
-    for (var i = 0; i < x_val; i++) {
-        if ((x_val - i) % 3 == 2) {
-            if (n_val[i] == '1') {
-                str_val += tn_val[Number(n_val[i + 1])] + ' ';
-                i++;
-                sk_val = 1;
-            } else if (n_val[i] != 0) {
-                str_val += tw_val[n_val[i] - 2] + ' ';
-                sk_val = 1;
-            }
-        } else if (n_val[i] != 0) {
-            str_val += dg_val[n_val[i]] + ' ';
-            if ((x_val - i) % 3 == 0)
-                str_val += 'hundred ';
-            sk_val = 1;
-        }
-        if ((x_val - i) % 3 == 1) {
-            if (sk_val)
-                str_val += th_val[(x_val - i - 1) / 3] + ' ';
-            sk_val = 0;
-        }
+module.exports = function numberToEnglish(n, custom_join_character) {
+    var string = n.toString(),
+    units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
+    var and = custom_join_character || 'and';
+    
+    if (parseInt(string) === 0) {
+    return 'zero';
     }
-    if (x_val != s.length) {
-        var y_val = s.length;
-        str_val += 'point ';
-        for (var i = x_val + 1; i < y_val; i++)
-            str_val += dg_val[n_val[i]] + ' ';
+    
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion'];
+    
+    start = string.length;
+    chunks = [];
+    while (start > 0) {
+    end = start;
+    chunks.push(string.slice((start = Math.max(0, start - 3)), end));
     }
-    return str_val.replace(/\s+/g, ' ');
-}
+    chunksLen = chunks.length;
+    if (chunksLen > scales.length) {
+    return '';
+    }
+
+    words = [];
+    for (i = 0; i < chunksLen; i++) {
+    chunk = parseInt(chunks[i]);
+    if (chunk) {
+    
+    ints = chunks[i].split('').reverse().map(parseFloat);
+    
+    if (ints[1] === 1) {
+    ints[0] += 10;
+    }
+    
+    if ((word = scales[i])) {
+    words.push(word);
+    }
+    
+    if ((word = units[ints[0]])) {
+    words.push(word);
+    }
+    
+    if ((word = tens[ints[1]])) {
+    words.push(word);
+    }
+    
+    if (ints[0] || ints[1]) {
+    
+    if (ints[2] || !i && chunksLen) {
+    words.push(and);
+    }
+    }
+    
+    if ((word = units[ints[2]])) {
+    words.push(word + ' hundred');
+    }
+    }
+    }
+    return words.reverse().join(' ');
+    }
